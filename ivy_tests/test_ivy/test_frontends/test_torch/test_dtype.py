dc8627c5e87e6d554101d7057b464882d5ef4cb8
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-08-15 19:41:03 +0530

    Update test_dtype.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_dtype.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_dtype.py
@@ -1,29 +1,46 @@
 # global
 from hypothesis import settings, strategies as st
 
-# local
+from hypothesis import settings, strategies as st
 import ivy
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers.testing_helpers import handle_frontend_test
 import ivy.functional.frontends.torch as torch_frontend
 
+# Define a common decorator for testing frontend functions
+def frontend_function_test(fn_tree, *args, **kwargs):
+    """
+    A decorator for testing frontend functions.
 
-# can_cast
-@handle_frontend_test(
+    Args:
+        fn_tree (str): Function tree path.
+        *args: Variable arguments.
+        **kwargs: Keyword arguments.
+
+    Returns:
+        callable: Decorator function.
+    """
+    def decorator(test_func):
+        @handle_frontend_test(fn_tree=fn_tree, *args, **kwargs)
+        @settings(max_examples=200)
+        def wrapped_test_function(*test_args, **test_kwargs):
+            return test_func(*test_args, **test_kwargs)
+        return wrapped_test_function
+    return decorator
+
+# Test torch.can_cast
+@frontend_function_test(
     fn_tree="torch.can_cast",
     from_=helpers.get_dtypes("valid", full=False),
     to=helpers.get_dtypes("valid", full=False),
     test_with_out=st.just(False),
     number_positional_args=st.just(2),
 )
-# there are 100 combinations of dtypes, so run 200 examples to make sure all are tested
-@settings(max_examples=200)
 def test_torch_can_cast(
     *,
     from_,
     to,
     on_device,
-    fn_tree,
     frontend,
     test_flags,
 ):
@@ -31,28 +48,24 @@ def test_torch_can_cast(
         input_dtypes=[],
         frontend=frontend,
         test_flags=test_flags,
-        fn_tree=fn_tree,
+        fn_tree="torch.can_cast",
         on_device=on_device,
         from_=ivy.Dtype(from_[0]),
         to=ivy.Dtype(to[0]),
     )
 
-
-# promote_types
-@handle_frontend_test(
+# Test torch.promote_types
+@frontend_function_test(
     fn_tree="torch.promote_types",
     type1=helpers.get_dtypes("valid", full=False),
     type2=helpers.get_dtypes("valid", full=False),
     test_with_out=st.just(False),
 )
-# there are 100 combinations of dtypes, so run 200 examples to make sure all are tested
-@settings(max_examples=200)
 def test_torch_promote_types(
     *,
     type1,
     type2,
     on_device,
-    fn_tree,
     frontend,
     test_flags,
 ):
@@ -60,7 +73,7 @@ def test_torch_promote_types(
         input_dtypes=[],
         frontend=frontend,
         test_flags=test_flags,
-        fn_tree=fn_tree,
+        fn_tree="torch.promote_types",
         on_device=on_device,
         test_values=False,
         type1=type1[0],
@@ -68,9 +81,8 @@ def test_torch_promote_types(
     )
     assert ret == repr(frontend_ret[0]).split(".")[1]
 
-
-# set_default_dtype
-@handle_frontend_test(
+# Test torch.set_default_dtype
+@frontend_function_test(
     fn_tree="torch.set_default_dtype",
     dtype=helpers.get_dtypes("float", full=False),
 )
@@ -81,3 +93,4 @@ def test_set_default_dtype(
     dtype = dtype[0]
     torch_frontend.set_default_dtype(dtype)
     assert torch_frontend.get_default_dtype() == dtype
+
