80fd214faf23ba876aad6f02de38c46210e85ed1
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-08-15 18:23:51 +0530

    Update setup_tests.py

--- setup_tests.py
+++ setup_tests.py
@@ -1,19 +1,30 @@
 import sys
 from run_tests_CLI.get_all_tests import BACKENDS
 
+def write_to_file(filename, content):
+    """
+    Writes the provided content to the specified file.
+    
+    Args:
+        filename (str): Name of the file to write to.
+        content (str): Content to write to the file.
+    """
+    with open(filename, "w") as file:
+        file.write(content)
 
 def main():
     if len(sys.argv) < 2:
         return
+
     test = sys.argv[1]
+    output_filename = "tests_to_run"
+
     if "," in test:
-        with open("tests_to_run", "w") as f:
-            f.write(test + "\n")
+        write_to_file(output_filename, test + "\n")
     else:
-        with open("tests_to_run", "w") as f:
+        with open(output_filename, "w") as f:
             for backend in BACKENDS:
                 f.write(f"{test},{backend}\n")
-
 
 if __name__ == "__main__":
     main()
