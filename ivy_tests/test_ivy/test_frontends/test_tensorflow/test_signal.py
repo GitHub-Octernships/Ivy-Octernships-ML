d03623f258619456bd67bbb6ffc470137797c980
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-08-16 23:20:40 +0530

    Update test_signal.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -1,25 +1,13 @@
-# global
-from hypothesis import strategies as st
 
-# local
-import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.helpers import handle_frontend_test
-
-
-# kaiser_window
-@handle_frontend_test(
-    fn_tree="tensorflow.signal.kaiser_window",
-    dtype_and_window_length=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("integer")
-    ),
-    dtype_and_beta=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric")
-    ),
-    dtype=helpers.get_dtypes("numeric"),
-    test_with_out=st.just(False),
+# Import Statements
+import hypothesis.strategies as st
+from ivy_tests.test_ivy.helpers import (
+    dtype_and_values, get_dtypes, ints, test_frontend_function
 )
+
+# Test Function Definitions
+# Kaiser Window Test
 def test_tensorflow_kaiser_window(
-    *,
     dtype_and_window_length,
     dtype_and_beta,
     dtype,
@@ -31,7 +19,7 @@ def test_tensorflow_kaiser_window(
 ):
     window_length_dtype, window_length = dtype_and_window_length
     beta_dtype, beta = dtype_and_beta
-    helpers.test_frontend_function(
+    test_frontend_function(
         input_dtypes=[window_length_dtype[0], beta_dtype[0]],
         backend_to_test=backend_fw,
         frontend=frontend,
@@ -43,36 +31,8 @@ def test_tensorflow_kaiser_window(
         dtype=dtype,
     )
 
-
-@st.composite
-def valid_idct(draw):
-    dtype, x = draw(
-        helpers.dtype_and_values(
-            available_dtypes=["float32", "float64"],
-            max_value=65280,
-            min_value=-65280,
-            min_num_dims=1,
-            min_dim_size=2,
-            shared_dtype=True,
-        )
-    )
-    n = None
-    axis = -1
-    norm = draw(st.sampled_from([None, "ortho"]))
-    type = draw(helpers.ints(min_value=1, max_value=4))
-    if norm == "ortho" and type == 1:
-        norm = None
-    return dtype, x, type, n, axis, norm
-
-
-# idct
-@handle_frontend_test(
-    fn_tree="tensorflow.signal.idct",
-    dtype_x_and_args=valid_idct(),
-    test_with_out=st.just(False),
-)
+# IDCT Test
 def test_tensorflow_idct(
-    *,
     dtype_x_and_args,
     frontend,
     test_flags,
@@ -81,7 +41,7 @@ def test_tensorflow_idct(
     on_device,
 ):
     input_dtype, x, type, n, axis, norm = dtype_x_and_args
-    helpers.test_frontend_function(
+    test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
         frontend=frontend,
@@ -96,26 +56,8 @@ def test_tensorflow_idct(
         atol=1e-01,
     )
 
-
-# dct
-@handle_frontend_test(
-    fn_tree="tensorflow.signal.dct",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=["float32", "float64"],
-        max_value=65280,
-        min_value=-65280,
-        min_num_dims=1,
-        min_dim_size=2,
-        shared_dtype=True,
-    ),
-    n=helpers.ints(min_value=1, max_value=3),
-    norm=st.sampled_from([None, "ortho"]),
-    type=helpers.ints(min_value=1, max_value=4),
-    # dtype_x_and_args=valid_idct(),
-    test_with_out=st.just(False),
-)
+# DCT Test
 def test_tensorflow_dct(
-    *,
     dtype_and_x,
     n,
     norm,
@@ -126,14 +68,11 @@ def test_tensorflow_dct(
     backend_fw,
     on_device,
 ):
-    (
-        input_dtype,
-        x,
-    ) = dtype_and_x
+    input_dtype, x = dtype_and_x
     if norm == "ortho" and type == 1:
         norm = None
     axis = -1
-    helpers.test_frontend_function(
+    test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
         frontend=frontend,
@@ -145,27 +84,19 @@ def test_tensorflow_dct(
         n=n,
         axis=axis,
         norm=norm,
-        # atol=1e-01,
     )
 
-
-# vorbis_window
-@handle_frontend_test(
-    fn_tree="tensorflow.signal.vorbis_window",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
-        max_num_dims=0,
-        min_value=1,
-        max_value=10,
-    ),
-    # dtype=helpers.get_dtypes("float", full=False),
-    test_with_out=st.just(False),
-)
+# Vorbis Window Test
 def test_tensorflow_vorbis_window(
-    *, dtype_and_x, test_flags, backend_fw, fn_tree, on_device, frontend  # ,dtype
+    dtype_and_x,
+    test_flags,
+    backend_fw,
+    fn_tree,
+    on_device,
+    frontend
 ):
     input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
+    test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
         frontend=frontend,
@@ -174,5 +105,24 @@ def test_tensorflow_vorbis_window(
         on_device=on_device,
         atol=1e-02,
         window_length=int(x[0]),
-        # dtype=dtype[0],
     )
+
+# Hypothesis Strategies
+valid_idct = st.composite(lambda draw: (
+    draw(dtype_and_values(
+        available_dtypes=["float32", "float64"],
+        max_value=65280,
+        min_value=-65280,
+        min_num_dims=1,
+        min_dim_size=2,
+        shared_dtype=True
+    )),
+    draw(st.sampled_from([-1, 0, 1])),  # type
+    None,  # n
+    -1,    # axis
+    draw(st.sampled_from([None, "ortho"]))  # norm
+))
+
+
+
+
